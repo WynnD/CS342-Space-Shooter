5c3109cb4b07025b4c69e8a8b01e833a5af3a0f7
NDolni2 <NDolni2@users.noreply.github.com>
2017-03-16 18:33:59 -0500

    Create Spaceship.java

---
+++ GalacticFX/Spaceship.java
@@ -0,0 +1,43 @@
+package Game;
+
+import javafx.scene.image.Image;
+import javafx.scene.canvas.GraphicsContext;
+
+
+public class Spaceship {
+
+    private Image spImage;
+    private int x, y, w, h;
+
+    public Spaceship(Image image, int x, int y, int w, int h)
+    {
+        this.spImage = image;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void drawShip(GraphicsContext gc)
+    {
+        gc.drawImage(spImage, x, y, w, h);
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
