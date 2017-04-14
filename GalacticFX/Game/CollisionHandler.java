2daccc9c7ce07ce75361b1826db943431e881803
NDolni2 <noemidolnik@yahoo.com>
2017-04-14 01:47:37 -0500

    CollisionHandler class updated, ship collisions fixed, projectile uses sprite

--- GalacticFX/Game/CollisionHandler.java
+++ GalacticFX/Game/CollisionHandler.java
@@ -1,65 +1,132 @@
 package Game;
 
 import java.awt.*;
+import java.io.File;
 import java.util.*;
-import javafx.scene.image.ImageView;
 import javafx.geometry.BoundingBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-/*
+
 public class CollisionHandler {
 
-    ArrayList<Spaceship> ships;
-    //thestage
-    //all panes
-    //window (for border collisions)
+    private ArrayList<Spaceship> ships;
+    private ArrayList<PlayerLife> lives;
+    private BoundingBox window;
 
-
-    public CollisionHandler(ArrayList<Spaceship> ships) {
+    public CollisionHandler(ArrayList<Spaceship> ships, BoundingBox window, ArrayList<PlayerLife> lives) {
         this.ships = ships;
+        this.window = window;
+        this.lives = lives;
     }
 
-    public boolean detectBorderCollision(int x, int y) {
-        if ((x >= w || x <= 0) || (y <= 0 || y >= h))
+    //returns index of ship collided with
+    public int checkShipCollisions(Coordinate2D newPosition, Spaceship ship)
+    {
+        int shipCollision = -1;
+        int curShipIndex = ships.indexOf(ship);
+        Spaceship curShip;
+
+        for(int i = 0; i < ships.size(); i++)
+        {
+            if(i != curShipIndex) {
+                curShip = ships.get(i);
+                //possibly make each ship have a boundingBox?
+                BoundingBox shipHitBox = new BoundingBox(curShip.getX(), curShip.getY(), curShip.getW(), curShip.getH());
+                if (shipHitBox.intersects(newPosition.getX(), newPosition.getY(), ship.getW(), ship.getH())) {
+                    shipCollision = i;
+                    return shipCollision;
+                }
+            }
+        }
+        return shipCollision;
+    }
+
+    public int checkProjectileCollisions(Coordinate2D newPosition, Spaceship ship)
+    {
+        int shipCollision = -1;
+        int curShipIndex = ships.indexOf(ship);
+        Spaceship curShip;
+
+        for(int i = 0; i < ships.size(); i++)
+        {
+            if(i != curShipIndex) {
+                curShip = ships.get(i);
+                //possibly make each ship have a boundingBox?
+                BoundingBox shipHitBox = new BoundingBox(curShip.getX(), curShip.getY(), curShip.getW(), curShip.getH());
+                if (shipHitBox.contains(newPosition.getX(), newPosition.getY())){
+                    shipCollision = i;
+                    return shipCollision;
+                }
+            }
+        }
+        return shipCollision;
+    }
+
+    public boolean shipInBounds(Coordinate2D newPosition, Spaceship ship)
+    {
+        if(window.contains(newPosition.getX(), newPosition.getY(), ship.getW(), ship.getH()))
+        {
             return true;
-        else
-            return false;
-    }
-
-    public boolean detectShipCollision(ImageView shipImage) {
-        for (Spaceship s : ships) {
-            if(s.getImageView())
         }
 
+        return false;
     }
 
-    public void handleUserHit(ArrayList<PlayerLife> lives)
+    public boolean projectileInBounds(Projectile projectile)
+    {
+        if(window.contains(projectile.getX(), projectile.getY()))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void handleShipCollision(int currentShip, int shipCollidedWith)
+    {
+        System.out.println("ship collision: " + currentShip + " collided with " + shipCollidedWith);
+        explosionSound();
+        if(currentShip == 0) {
+            handleUserHit();
+            ships.get(shipCollidedWith).destroy();
+        }
+        else if(shipCollidedWith == 0){
+            handleUserHit();
+            ships.get(currentShip).destroy();
+        }
+
+        //OPTIONS: either game over OR remove ship and take life away
+    }
+
+
+    public void handleProjectileCollision(Spaceship s, Projectile p, int shipIndex)
+    {
+        s.getProjectiles().remove(p);
+        explosionSound();
+        if(shipIndex == 0)
+        {
+            handleUserHit();
+        }
+        else {
+            ships.remove(shipIndex);
+        }
+    }
+
+    public void handleUserHit()
     {
         System.out.println("User ship hit");
-        lives.remove(lives.size()-1);
+        lives.remove(lives.size() - 1);
         System.out.println("num lives: " + lives.size());
-
-        if(lives.size() == 0) {
-           setGameOverScreen();
-        }
     }
 
-    public void setGameOverScreen()
+    public void explosionSound()
     {
-        Label loseLabel = new Label("GAME OVER");
-        loseLabel.setTextFill(Color.RED);
-        Font font = new Font("Arial", 70);
-        loseLabel.setFont(font);
-        gameOver.setCenter(loseLabel);
-        theStage.setScene(endScene);
+        String musicFile2 = "Music/boom.mp3";
+        Media sound2 = new Media(new File(musicFile2).toURI().toString());
+        MediaPlayer mediaPlayer2 = new MediaPlayer(sound2);
+        mediaPlayer2.play();
     }
-
-    public void handleShipCollision()
-    {
-
-    }
-
-
 
 }
 
-*/
